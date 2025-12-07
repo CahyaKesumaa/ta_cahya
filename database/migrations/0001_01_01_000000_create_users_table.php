8dc533873ffa0e2d26fe9d856efa8d6fa2cb465c
Cahyakesumaa <220030604@stikom-bali.ac.id>
2025-12-07 18:13:41 +0800

    fix database: migration, seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,15 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
+            $table->boolean('is_active')->default(false);
+            $table->string('no_hp');
             $table->string('password');
+            $table->timestamp('verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
 
+        // Ubah sistem reset password menggunakan username
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('username')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
