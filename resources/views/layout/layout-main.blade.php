409f7b0e56f57aa3e4b0d02ffef15fe3712b2a3b
ardi-juniawan <ardijuniawan698@gmail.com>
2026-04-20 19:58:36 +0800

    chore: update default page title to Sharefit in layout views

--- resources/views/layout/layout-main.blade.php
+++ resources/views/layout/layout-main.blade.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" href="{{ asset('img/sf_logo.png') }}">
-    <title>@yield('page-title', 'Sharefit')</title>
     <title>@yield('page-title', 'Sharefit')</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
@@ -14,9 +11,9 @@
 
 <body>
     @include('layout.partials.navbar')
-    @yield('content')
-    @include('layout.partials.footer')
+    @yield('main')
     @yield('script')
+    @include('layout.partials.footer')
 </body>
 
 </html>
